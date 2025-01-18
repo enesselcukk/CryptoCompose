8cd291c4224ff65d3f78389c5ddb6549b5272483
enes selçuk <enesselcu25@gmail.com>
2025-01-18 16:31:06 +0300

    fix

--- core/src/main/java/com/example/btccompose/core/utils/NetworkState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.btccompose.utils
-
-sealed class NetworkState<out T> {
-    data class Success<out T>(val data: T) : NetworkState<T>()
-    data class Error<T>(val message: Exception?) : NetworkState<T>()
-    data object Loading : NetworkState<Nothing>()
-    data object Completed : NetworkState<Nothing>()
-}
--- domain/src/main/java/com/example/btccompose/domain/model/ErrorState.kt
+++
@@ -1,6 +0,0 @@
-package com.example.btccompose.domain.model
-
-sealed class ErrorState {
-    data class NetworkError(val message: String) : ErrorState()
-    data class GeneralError(val exception: Exception) : ErrorState()
-} 
